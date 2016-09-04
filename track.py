004a898d0d9626bb526a46d5b207b44529905ab8
Franklin Raccah <fraccah@f.reckl.es>
2016-09-04 16:04:31 +0200

    Made the track an Object

--- track.py
+++ track.py
@@ -2,6 +2,7 @@
 from __future__ import division
 from PIL import Image, ImageDraw
 from math import pi, cos, sin
+from collections import Iterable
 
 # Sizes are in feet
 # Pivot line interior is 0
@@ -14,208 +15,191 @@ LINES = "magenta"
 BACK = "beige"
 FRONT = "white"
 
-def _track_curves(draw):
-    left_out_curve = (((108/2)-17.5-26.5) * S,
-                      ((75/2)+1-26.5) * S,
-                      ((108/2)-17.5+26.5) * S,
-                      ((75/2)+1+26.5) * S)
-    draw.pieslice(left_out_curve, 90, 270, FRONT, OUTLINE)
-    right_out_curve = (((108/2)+17.5-26.5) * S,
-                       ((75/2)-1-26.5) * S,
-                       ((108/2)+17.5+26.5) * S,
-                       ((75/2)-1+26.5) * S)
-    draw.pieslice(right_out_curve, 270, 450, FRONT, OUTLINE)
-    left_in_curve = (((108/2)-17.5-12.5) * S,
-                      ((75/2)-12.5) * S,
-                      ((108/2)-17.5+12.5) * S,
-                      ((75/2)+12.5) * S)
-    draw.pieslice(left_in_curve, 90, 270, BACK, OUTLINE)
-    right_in_curve = (((108/2)+17.5-12.5) * S,
-                      ((75/2)-12.5) * S,
-                      ((108/2)+17.5+12.5) * S,
-                      ((75/2)+12.5) * S)
-    draw.pieslice(right_in_curve, 270, 450, BACK, OUTLINE)
-
-def _track_straights_clean(draw):
-    top_out_left = (((108/2)-17.5) * S,
-                    ((75/2)-12.5-13) * S)
-    top_out_right = (((108/2)+17.5) * S,
-                     ((75/2)-12.5-15) * S)
-    top_in_left = ((108/2-17.5) * S,
-                   (75/2-12.5) * S)
-    top_in_right = (((108/2)+17.5) * S,
-                    ((75/2)-12.5) * S)
-
-    bottom_in_left = (((108/2)-17.5) * S,
-                      ((75/2)+12.5) * S)
-    bottom_in_right = (((108/2)+17.5) * S,
-                       ((75/2)+12.5) * S)
-    bottom_out_left = ((108/2-17.5) * S,
-                       (75/2+12.5+15) * S)
-    bottom_out_right = (((108/2)+17.5) * S,
-                        ((75/2)+12.5+13) * S)
-
-
-    top_straight = (top_out_left,
-                    top_out_right,
-                    top_in_right,
-                    top_in_left)
-    draw.polygon(top_straight, FRONT, OUTLINE)
-    bottom_straight = (bottom_in_left,
-                       bottom_in_right,
-                       bottom_out_right,
-                       bottom_out_left)
-    draw.polygon(bottom_straight, FRONT, OUTLINE)
-
-    # Let's erase black lines by putting colored lines on top
-    draw.line((top_out_left, top_in_left), FRONT)
-    draw.line((top_out_right, top_in_right), FRONT)
-    draw.line((bottom_in_left, bottom_out_left), FRONT)
-    draw.line((bottom_in_right, bottom_out_right), FRONT)
-    draw.line((top_in_left, bottom_in_left), BACK)
-    draw.line((top_in_right, bottom_in_right), BACK)
-
 def get_approx(x, x_beg, x_end, max_beg, max_end):
     deg = (x - x_beg) / (x_end - x_beg)
 
-def get_xy(x_int, pos=0, shift=0):
-    """`x_int` is the inside distance from the start
-       `pos` is position on the track. 0.0 is inside. 1.0 is outside
-       `shift` is the shift position (if you want the track to start from the
-       jammer line, us a `shift` of -30)"""
-    pos_x = 0
-    pos_y = 0
-    end_first_curve = pi * 12.5
-    end_first_line = end_first_curve + 35
-    end_second_curve = end_first_line + pi * 12.5
-    end_second_line = end_second_curve + 35
-    x_int += shift
-    x_int %= end_second_line
+class Track(object):
+    def __init__(self):
+        self.scale = 10
+        self.len = 108
+        self.wid = 75
+        self.mark_dist = 17.5
+        self.int_radius = 12.5
+        self.ext_radius = 26.5
+        self.int_ext_shift = 1
+        self.small_breadth = 13
+        self.large_breadth = 15
 
-    lim_down = 0
-    lim_up = end_first_curve
-    ref_x = (108/2)-17.5
-    ref_y = (75/2)
-    min_width = 13
-    max_width = 15
-    angle_shift = -(pi/2)
-    curve = True
-    if end_first_curve <= x_int < end_first_line:
-        # First line TODO
-        print "Bottom line"
-        lim_down = end_first_curve
-        lim_up = end_first_line
+    def draw(self, img):
+        self.img = img
+        self._draw_curves()
+        self._draw_straights()
+
+    def real_dim(self, dim):
+        if isinstance(dim, Iterable):
+            real_dim = tuple(self.real_dim(x) for x in dim)
+        else:
+            real_dim = self.scale*dim
+
+        return real_dim
+
+    def _draw_curves(self):
+        img = self.img
+        left_out_curve = (((self.len/2)-self.mark_dist-self.ext_radius),
+                          ((self.wid/2)+self.int_ext_shift-self.ext_radius),
+                          ((self.len/2)-self.mark_dist+self.ext_radius),
+                          ((self.wid/2)+self.int_ext_shift+self.ext_radius))
+
+        right_out_curve = (((self.len/2)+self.mark_dist-self.ext_radius),
+                           ((self.wid/2)-self.int_ext_shift-self.ext_radius),
+                           ((self.len/2)+self.mark_dist+self.ext_radius),
+                           ((self.wid/2)-self.int_ext_shift+self.ext_radius))
+
+        left_in_curve = (((self.len/2)-self.mark_dist-self.int_radius),
+                          ((self.wid/2)-self.int_radius),
+                          ((self.len/2)-self.mark_dist+self.int_radius),
+                          ((self.wid/2)+self.int_radius))
+
+        right_in_curve = (((self.len/2)+self.mark_dist-self.int_radius),
+                          ((self.wid/2)-self.int_radius),
+                          ((self.len/2)+self.mark_dist+self.int_radius),
+                          ((self.wid/2)+self.int_radius))
+
+        img.pieslice(self.real_dim(left_out_curve), 90, 270, FRONT, OUTLINE)
+        img.pieslice(self.real_dim(right_out_curve), 270, 450, FRONT, OUTLINE)
+        img.pieslice(self.real_dim(left_in_curve), 90, 270, BACK, OUTLINE)
+        img.pieslice(self.real_dim(right_in_curve), 270, 450, BACK, OUTLINE)
+
+    def _draw_straights(self):
+        img = self.img
+        top_out_left = (((self.len/2)-self.mark_dist),
+                        ((self.wid/2)-self.int_radius-self.small_breadth))
+        top_out_right = (((self.len/2)+self.mark_dist),
+                         ((self.wid/2)-self.int_radius-self.large_breadth))
+        top_in_left = ((self.len/2-self.mark_dist),
+                       (self.wid/2-self.int_radius))
+        top_in_right = (((self.len/2)+self.mark_dist),
+                        ((self.wid/2)-self.int_radius))
+
+        bottom_in_left = (((self.len/2)-self.mark_dist),
+                          ((self.wid/2)+self.int_radius))
+        bottom_in_right = (((self.len/2)+self.mark_dist),
+                           ((self.wid/2)+self.int_radius))
+        bottom_out_left = ((self.len/2-self.mark_dist),
+                           (self.wid/2+self.int_radius+self.large_breadth))
+        bottom_out_right = (((self.len/2)+self.mark_dist),
+                            ((self.wid/2)+self.int_radius+self.small_breadth))
+
+
+        top_straight = (top_out_left,
+                        top_out_right,
+                        top_in_right,
+                        top_in_left)
+        bottom_straight = (bottom_in_left,
+                           bottom_in_right,
+                           bottom_out_right,
+                           bottom_out_left)
+
+        img.polygon(self.real_dim(top_straight), FRONT, OUTLINE)
+        img.polygon(self.real_dim(bottom_straight), FRONT, OUTLINE)
+
+        # Let's erase black lines by putting colored lines on top
+        img.line(self.real_dim((top_out_left, top_in_left)), FRONT)
+        img.line(self.real_dim((top_out_right, top_in_right)), FRONT)
+        img.line(self.real_dim((bottom_in_left, bottom_out_left)), FRONT)
+        img.line(self.real_dim((bottom_in_right, bottom_out_right)), FRONT)
+        img.line(self.real_dim((top_in_left, bottom_in_left)), BACK)
+        img.line(self.real_dim((top_in_right, bottom_in_right)), BACK)
+
+    def get_xy(x, pos=0.5, shift=0):
+        """`x` is the middle distance from the start
+           `pos` is position on the track. 0.0 is inside. 1.0 is outside
+           `shift` is the shift position (if you want the track to start from the
+           jammer line, us a `shift` of -30)"""
+        pos_x = 0
+        pos_y = 0
+        end_first_curve = pi * 12.5
+        end_first_line = end_first_curve + 35
+        end_second_curve = end_first_line + pi * 12.5
+        end_second_line = end_second_curve + 35
+
+        end_of_track = 0
+        x += shift
+        x %= end_of_track
+
+        lim_down = 0
+        lim_up = end_first_curve
+        min_width = 13
+        max_width = 15
         ref_x = (108/2)-17.5
         ref_y = (75/2)
-        dim_shift = 1
-        curve = False
-    elif end_first_line <= x_int < end_second_curve:
-        # Second curve TODO
-        print "Right curve"
-        lim_down = end_first_line
-        lim_up = end_second_curve
-        ref_x = (108/2)+17.5
-        angle_shift = pi/2
+        angle_shift = -(pi/2)
         curve = True
-    elif end_second_curve <= x_int < end_second_line:
-        # Second line TODO
-        print "Top line"
-        lim_down = end_second_curve
-        lim_up = end_second_line
-        ref_x = (108/2)+17.5
-        ref_y = (75/2)
-        dim_shift = -1
-        curve = False
-    else:
-        # First curve
-        print "Left curve"
+        if end_first_curve <= x_int < end_first_line:
+            # First line TODO
+            print "Bottom line"
+            lim_down = end_first_curve
+            lim_up = end_first_line
+            ref_x = (108/2)-17.5
+            ref_y = (75/2)
+            dim_shift = 1
+            curve = False
+        elif end_first_line <= x_int < end_second_curve:
+            # Second curve TODO
+            print "Right curve"
+            lim_down = end_first_line
+            lim_up = end_second_curve
+            ref_x = (108/2)+17.5
+            angle_shift = pi/2
+            curve = True
+        elif end_second_curve <= x_int < end_second_line:
+            # Second line TODO
+            print "Top line"
+            lim_down = end_second_curve
+            lim_up = end_second_line
+            ref_x = (108/2)+17.5
+            ref_y = (75/2)
+            dim_shift = -1
+            curve = False
+        else:
+            # First curve
+            print "Left curve"
 
-    prop = (x_int - lim_down) / (lim_up - lim_down)
-    ref_len_min = 12.5
+        prop = (x_int - lim_down) / (lim_up - lim_down)
+        ref_len_min = 12.5
 
-    if curve:
-        ref_len_max = 12.5 + (((max_width - min_width) * prop) + min_width)
-        ref_len = ((ref_len_max - ref_len_min) * pos) + ref_len_min
-        angle = (prop * -2 * pi)/2 + angle_shift
-        # Point defined by the arc of angle
-        # of the circle of center (ref_x, ref_y) and radius ref_len
-        pos_x = ref_len * cos(angle) + ref_x
-        pos_y = ref_len * sin(angle) + ref_y
-    else:
-        ref_len_max = 12.5 + (((max_width - min_width) * (1-prop)) + min_width)
-        ref_len = ((ref_len_max - ref_len_min) * pos * dim_shift) + ref_len_min * dim_shift
-        pos_x = ref_x + (prop * 35 * dim_shift)
-        pos_y = ref_y + ref_len
-    return pos_x, pos_y
+        if curve:
+            ref_len_max = 12.5 + (((max_width - min_width) * prop) + min_width)
+            ref_len = ((ref_len_max - ref_len_min) * pos) + ref_len_min
+            angle = (prop * -2 * pi)/2 + angle_shift
+            # Point defined by the arc of angle
+            # of the circle of center (ref_x, ref_y) and radius ref_len
+            pos_x = ref_len * cos(angle) + ref_x
+            pos_y = ref_len * sin(angle) + ref_y
+        else:
+            ref_len_max = 12.5 + (((max_width - min_width) * (1-prop)) + min_width)
+            ref_len = ((ref_len_max - ref_len_min) * pos * dim_shift) + ref_len_min * dim_shift
+            pos_x = ref_x + (prop * 35 * dim_shift)
+            pos_y = ref_y + ref_len
+        return pos_x, pos_y
 
-def _track_lines(draw):
-    pivot_in_x, pivot_in_y = get_xy(0, 0, shift=0)
-    pivot_out_x, pivot_out_y = get_xy(0, 1, shift=0)
-
-    draw.line(((pivot_in_x * S,
-                pivot_in_y * S),
-               (pivot_out_x * S,
-                pivot_out_y * S)), LINES)
-
-    jammer_in_x, jammer_in_y = get_xy(0, 0, shift=-30)
-    jammer_out_x, jammer_out_y = get_xy(0, 1, shift=-30)
-
-    draw.line(((jammer_in_x * S,
-                jammer_in_y * S),
-               (jammer_out_x * S,
-                jammer_out_y * S)), LINES)
-
-
-def _track_marks(draw):
-    for i in range(1, 12):
-        mark_in_x, mark_in_y = get_xy(i * 10, 0.4, shift=0)
-        mark_out_x, mark_out_y = get_xy(i * 10, 0.6, shift=0)
-        draw.line(((mark_in_x * S,
-                    mark_in_y * S),
-                   (mark_out_x * S,
-                    mark_out_y * S)), LINES, width=3)
-
-def _draw_player(draw, x, y, size=3, color="yellow", outline="black"):
-    draw.ellipse((((x*S)-(size*S/2)),
-                  ((y*S)-(size*S/2)),
-                  ((x*S)+(size*S/2)),
-                  ((y*S)+(size*S/2))),
-                  fill=color,
-                  outline=outline)
-
-def draw_track(draw):
-    """Cf. Appendix A"""
-    _track_curves(draw)
-    _track_straights_clean(draw)
-    _track_lines(draw)
-    _track_marks(draw)
+#def _draw_player(draw, x, y, size=3, color="yellow", outline="black"):
+#    draw.ellipse((((x*S)-(size*S/2)),
+#                  ((y*S)-(size*S/2)),
+#                  ((x*S)+(size*S/2)),
+#                  ((y*S)+(size*S/2))),
+#                  fill=color,
+#                  outline=outline)
 
 def main():
-    im = Image.new('RGBA', (108*S, 75*S), BACK)
+    im = Image.new('RGBA', (108*10, 75*10), BACK)
     draw = ImageDraw.Draw(im)
-    draw_track(draw)
+    track = Track()
+    track.draw(draw)
 
     # Jammers
-    x, y = get_xy(0, 0.8, shift=30)
-    _draw_player(draw, x, y, color="grey")
-    x, y = get_xy(7.5, 0.4, shift=30)
-    _draw_player(draw, x, y)
-
-    x, y = get_xy(2, 0.85, shift=30)
-    _draw_player(draw, x, y)
-    x, y = get_xy(2, 0.65, shift=30)
-    _draw_player(draw, x, y)
-    x, y = get_xy(3.5, 0.75, shift=30)
-    _draw_player(draw, x, y)
-
-    x, y = get_xy(10, 0.25, shift=30)
-    _draw_player(draw, x, y, color="grey")
-    x, y = get_xy(10, 0.45, shift=30)
-    _draw_player(draw, x, y, color="grey")
-    x, y = get_xy(13, 0.25, shift=30)
-    _draw_player(draw, x, y, color="grey")
-    x, y = get_xy(13, 0.45, shift=30)
-    _draw_player(draw, x, y, color="grey")
+#    x, y = get_xy(0, 0, shift=0)
+#    _draw_player(draw, x, y, color="grey")
 
     im.show()
 
